bd218957b29206f0af2bc06c32bf8be34ca6684f
julesdocx <jules.docx@student.ehb.be>
2022-05-09 21:22:11 +0200

    serve styles

--- api/lib/controllers/versions.controller.js
+++ api/lib/controllers/versions.controller.js
@@ -1,9 +1,17 @@
 const express = require("express");
 let router = express.Router();
+const fs = require("fs");
 
 router.get("/v0", (req, res) => {
   console.log("insert styles here")
-  res.status(200).send("gucci")
+        res.writeHead(200, { "Content-type": "text/css" });
+        let fileContents = fs.readFileSync(`${process.cwd()}/css/index.css`, {
+          encoding: "utf8",
+        });
+        res.write(fileContents);
+        res.end();
 });
+
+
 
 module.exports = router; 
