a5c31696abc7cdbc9d62ccd321dc98540710ece8
julesdocx <jules.docx@student.ehb.be>
2022-05-12 00:27:45 +0200

    gulp clean css minify addon

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const purgecss = require('gulp-purgecss')
+const cleanCSS = require("gulp-clean-css");
+
+
 
 const buildStyles = () => {
-    return src("lib/**/*.scss")
+    return (
+      src("lib/**/*.scss")
         .pipe(sass())
+        .pipe(cleanCSS({ compatibility: "ie8" }))
         //.pipe(purgecss({ content: ['*.html'] }))
-        .pipe(dest("css"));
+        .pipe(dest("css"))
+    );
 }
 
 const watchTask = () => watch(["*/**/*.scss", '*html'], buildStyles);
