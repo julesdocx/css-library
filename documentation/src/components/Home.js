963fe1c3742f7a4fdbc2b3a2fbdda1a16f370e4c
julesdocx <jules.docx@student.ehb.be>
2022-06-07 23:12:43 +0200

    homepage

--- documentation/src/components/Home.js
+++ documentation/src/components/Home.js
@@ -1,37 +1,63 @@
 import { gsap } from "gsap";
 import React, { useEffect, useRef } from "react";
 import { Link, Outlet } from "react-router-dom";
-
+import "./home.css"
 const Home = () => {
 
 const title = useRef();
+const lazy = useRef();
+const description = useRef();
 
-useEffect(() => {
-  gsap.from(".title", {
-    duration: 0.8,
-    delay: 0.4,
-    opacity: 0,
-    ease: "power3.out",
-    x: 100,
+    useEffect(() => {
+    gsap.to(title.current, {
+      delay: 0.2,
+      opacity: "100",
+      ease: "poawer2.out",
+      duration: 1,
+      x: -100,
+    });
+     gsap.to(lazy.current, {
+       delay: 0.8,
+       opacity: "100",
+       ease: "poawer2.out",
+       duration: 1,
+       x: 270,
+     });
+      gsap.to(description.current, {
+        delay: 1,
+        opacity: "100",
+        ease: "poawer2.out",
+        duration: 1,
+        x: -100,
+      });
   });
-});
+
 
   return (
-    <div className="span-12x1">
-      <h1 className="font-96 opacity-50 spacing-sm title">Begone CSS</h1>
-      <div className="display-f direction-c align-i-s">
-      <Link
-        to="/gettingstarted"
-        className="btn bg-light-grey hover-white text-blue"
-        >
-        Getting started
-      </Link>
-      <Link
-        to="/documentation"
-        className="btn bg-light-grey text-blue hover-white mt-12"
-        >
-        Documentation
-      </Link>
+    <div>
+      <div className="mt-60 direction-c align-i-c align-c md-grid grid-12">
+        <div className="span-5x1">
+          <img
+            className="lazy-img"
+            ref={lazy}
+            src="./lazy_woman.jpg"
+            alt="lazy baby"
+          />
+        </div>
+        <div className="mt-60 pt-60 span-12x1">
+          <img
+            src="./Asset 3.svg"
+            alt="handmade"
+            ref={title}
+            className="opacity-100 mt-60 title-img"
+          />
+        </div>
+        <div ref={description} className="description mt-12 span-5x1 grid-start-6 align-l text-grey">
+          <p>
+              CSS library with a focus on helping people with the first steps of
+              web-developent
+          </p>
+        </div>
       </div>
     </div>
   );
--- documentation/src/components/Nav.js
+++ documentation/src/components/Nav.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import "./nav.css"
 
 function Nav() {
 //const [isActive, setTabState1] = useState(0);
@@ -40,13 +41,13 @@ const navigate = useNavigate();
         isShrunk ? "shrunkNav" : "main-nav"
       } navbar-fixed bg-white lg`}
     >
-      <div className="grid-10 lg-grid align-i-s tab-group">
+      <div className="grid-8 lg-grid align-i-s tab-group">
         <NavLink
           to="/"
           className={({ isActive }) =>
-            (isActive
-              ? "active span-6x1 font-bold active-tab flex-g"
-              : "span-2x1")
+            isActive
+              ? "span-2x1 font-bold active-tab pt-4 mt-24 navlink flex-g"
+              : "span-2x1 font-bold text-grey pt-4 mt-24 navlink"
           }
         >
           Begone CSS
@@ -54,22 +55,32 @@ const navigate = useNavigate();
         <NavLink
           to="/documentation"
           className={({ isActive }) =>
-            (isActive
-              ? " span-6x1 font-bold active-tab flex-g"
-              : "span-2x1")
+            isActive
+              ? " span-2x1 active-tab flex-g pt-4 mt-24 navlink font-bold"
+              : "span-2x1 font-bold text-grey pt-4 mt-24 navlink"
           }
         >
-          Documentation
+          Components
         </NavLink>
         <NavLink
           to="/gettingstarted"
           className={({ isActive }) =>
-            (isActive
-              ? "span-6x1 font-bold active-tab flex-g"
-              : "span-2x1")
+            isActive
+              ? "span-2x1 active-tab flex-g pt-4 mt-24 navlink font-bold"
+              : "span-2x1 font-bold text-grey pt-4 mt-24 navlink"
           }
         >
           Getting started
+        </NavLink>
+        <NavLink
+          to="/gettingstarted"
+          className={({ isActive }) =>
+            isActive
+              ? "span-2x1 active-tab flex-g pt-4 mt-24 navlink font-bold"
+              : "span-2x1 font-bold text-grey pt-4 mt-24 navlink"
+          }
+        >
+          Darkmode
         </NavLink>
       </div>
     </nav>
